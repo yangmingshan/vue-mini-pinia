8a80f7716a4e5b3f80afe85e5a08f8754b1d7c13
Yang Mingshan <y.mingshan3@gmail.com>
2025-01-01 21:12:00 +0800

    update

--- src/pages/mine/index.ts
+++ src/pages/mine/index.ts
@@ -1,9 +1,17 @@
 import { defineComponent, ref } from '@vue-mini/core';
+import { storeToRefs } from '@vue-mini/pinia';
+import { useStore } from '@/pinia';
 
 defineComponent(() => {
+  const store = useStore();
+  const { increment } = store;
   const greeting = ref('希望你会喜欢');
 
+  const { count, double } = storeToRefs(store);
   return {
+    count,
+    double,
     greeting,
+    increment,
   };
 });
