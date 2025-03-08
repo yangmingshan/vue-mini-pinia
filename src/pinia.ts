0c97238efcfcbea3b047ddf97eea857e503ad5a6
Yang Mingshan <y.mingshan3@gmail.com>
2025-03-08 16:59:20 +0800

    feat: use pinia-mini-plugin-persistor

--- src/pinia.ts
+++ src/pinia.ts
@@ -1,11 +1,20 @@
 import { ref, computed } from '@vue-mini/core';
 import { createPinia, defineStore } from '@vue-mini/pinia';
+import persistor from 'pinia-mini-plugin-persistor';
 
 export const pinia = createPinia();
 
-export const useStore = defineStore('main', () => {
-  const count = ref(0);
-  const double = computed(() => count.value * 2);
-  const increment = () => count.value++;
-  return { count, double, increment };
-});
+pinia.use(persistor);
+
+export const useStore = defineStore(
+  'main',
+  () => {
+    const count = ref(0);
+    const double = computed(() => count.value * 2);
+    const increment = () => count.value++;
+    return { count, double, increment };
+  },
+  {
+    persist: true,
+  },
+);
